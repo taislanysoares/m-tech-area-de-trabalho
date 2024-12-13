b575dd5e1d47dc206a7b1060432e3f1613775e31
_tais <carvalhotaislany@gmail.com>
2024-12-13 17:26:02 -0300

    Add files via upload

---
+++ Nova taislany carvalho soares turma 16 as18/web desing/aula001.js
@@ -0,0 +1,9 @@
+// let num1 = 10;
+// let num2 = 20;
+// let soma = num1 + num2 ;
+// console.log("a sema é: + soma");
+
+function tudobem(nome) {
+    return "olá " + nome;
+}
+console.log(tudobem("tais"));
